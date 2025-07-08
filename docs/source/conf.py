55329afaf39e0e802da17be26b774a8288bd3499
angushushu <angushushu@outlook.com>
2025-07-08 23:21:04 +0800

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,59 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+import os
+import sys
+# 修复路径：添加包含ciga包的父目录
+sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+import ciga.analysis.graph_centrality_analysis
+import ciga.analysis.graph_community_analysis
+import ciga.analysis.tgraph_centrality_analysis
+import ciga.analysis.tgraph_community_analysis
+import ciga.analysis.graph_properties
+import ciga.analysis.tgraph_properties
 
-release = '0.1'
-version = '0.1.0'
+import ciga.visualization.graph_plotter
 
-# -- General configuration
+import ciga.utils.data_utils
+import ciga.utils.graph_utils
 
+import ciga.ciga
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CIGA'
+copyright = '2025, Shu Hu'
+author = 'Shu Hu'
+release = '2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# 添加必要的sphinx扩展
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',  # 支持Google和NumPy风格的docstring
+    'sphinx_rtd_theme',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# autodoc配置
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
 }
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,22 +1,24 @@
-Welcome to Lumache's documentation!
-===================================
+.. CIGA documentation master file, created by
+   sphinx-quickstart on Tue Jul  8 21:42:36 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-**Lumache** (/lu'make/) is a Python library for cooks and food lovers
-that creates recipes mixing random ingredients.
-It pulls data from the `Open Food Facts database <https://world.openfoodfacts.org/>`_
-and offers a *simple* and *intuitive* API.
+CIGA documentation
+==================
+CIGA is a Python package designed for performing graph analysis on social interactions between individuals across time. It is a redesign of CharNet using igraph.
 
-Check out the :doc:`usage` section for further information, including
-how to :ref:`installation` the project.
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
 
-.. note::
-
-   This project is under active development.
-
-Contents
---------
 
 .. toctree::
+   :maxdepth: 2
+   :caption: Contents:
 
-   usage
-   api
+   data
+   graph
+   analysis
+   visualization
+   Legal
+   tutorials
